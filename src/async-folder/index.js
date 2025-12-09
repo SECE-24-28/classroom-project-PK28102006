1d43be1f47ec4e587fc2b8cc1362d77b04ce78c9
Poovendrakumar <poovendrakumart2006@gmail.com>
2025-12-09 10:14:46 +0530

    multiple api added

--- src/async-folder/index.js
+++ src/async-folder/index.js
@@ -6,9 +6,63 @@
 
 //Syncronous
 
-console.log("Before");
+// console.log("Before");
 
-setTimeout(() => {
-  console.log("Hello");
-}, 5000);                    //ASYNCHRONOUS
-console.log("After");
+// setTimeout(() => {                // Asynchronous
+//   console.log("Hello");
+// }, 5000);                   
+// console.log("After");
+
+
+//  async function fetchApi(){
+//     const response=await fetch("https://jsonplaceholder.typicode.com/users");
+//     const ans=await response.json();
+//     console.log("The user:",ans);
+
+// }
+// fetchApi();
+import axios from "axios";
+
+//  async function fetchApi() {
+//    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//    const ans = await response.json();
+//    console.log("The user:", ans);
+//  }
+//  fetchApi();
+
+// async function fetchApi() {
+//   const response = await axios("https://jsonplaceholder.typicode.com/users");
+//   console.log("The user:", response);
+// }
+// fetchApi();
+
+// async function fetchApi() {
+//   try {
+//     const response = await axios("https://jsonplaceholder.typicode.com/users");
+//     console.log("The user:", response);
+//   } catch (error) {
+//     console.log("Error", error);
+//   }
+// }
+
+// fetchApi();
+
+// https://dummyjson.com/products
+
+// https://dummyjson.com/todos
+
+// https://dummyjson.com/users
+
+async function fetchMultipleApis() {
+  let urls = [
+    "https://dummyjson.com/todos",
+    "https://dummyjson.com/todos",
+  ];
+  let promises = urls.map((item) => fetch(item).then((res) => res.json()));
+  let data = await Promise.all(promises);
+  console.log("The data is printed:", data);
+
+  //   console.log("Promises:", promises);
+}
+
+fetchMultipleApis();
